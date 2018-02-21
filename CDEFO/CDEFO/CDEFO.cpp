3b431358af3c470e1567495069cc4ff0fc1a30fc
TeleBooth <jbooth1@terpmail.umd.edu>
2018-02-21 02:56:29 -0500

    mo' edits
    I think I'm starting to figure ou the quirks of getting these things to run.

--- CDEFO/CDEFO/CDEFO.cpp
+++ CDEFO/CDEFO/CDEFO.cpp
@@ -27,11 +27,11 @@ void cdefo::play_audio(char location[])
 
 void cdefo::chase(Adafruit_NeoPixel *strip, uint32_t c)
 {
-	for (uint16_t i = 0; i < strip.numPixels() + 4; i++)
+	for (uint16_t i = 0; i < strip->numPixels() + 4; i++)
 	{
-		strip.setPixelColor(i, c); // Draw new pixel
-		strip.setPixelColor(i - 4, 0); // Erase pixel a few steps back
-		strip.show();
+		strip->setPixelColor(i, c); // Draw new pixel
+		strip->setPixelColor(i - 4, 0); // Erase pixel a few steps back
+		strip->show();
 		delay(25);
 	}
 }
