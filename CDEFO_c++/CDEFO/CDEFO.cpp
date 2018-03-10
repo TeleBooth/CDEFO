71e43ccce6ac35a7f146f44ed6fc5b3c15acae69
TeleBooth <jbooth1@terpmail.umd.edu>
2018-03-09 21:31:35 -0500

    MAJOR OVERHAUL
    ditched the whole concept of static, CDEFO is now able to be instantiated. Otherwise, I wouldn't have been able to add concurrency.
    
    I might have to revert all of this, but it seems to work alright enough. the only issue appears to be the fact that I'm doing so many String operations, so I have to work on transforming those into byte/char arrays so that I don't run out of RAM.

--- CDEFO_c++/CDEFO/CDEFO.cpp
+++ CDEFO_c++/CDEFO/CDEFO.cpp
@@ -1,16 +1,63 @@
 #include "CDEFO.h"
 
 
-cdefo::cdefo() = default;
+cdefo::cdefo(NfcTag* tag)
+{
+	message = tag->getNdefMessage();
+	record_count = message.getRecordCount();
+	//music = Adafruit_NeoPixel(N_LEDS, MUSIC_PIN, NEO_GRB + NEO_KHZ800);
+	mood_strip = Adafruit_NeoPixel(N_LEDS, MOOD_PIN, NEO_GRB + NEO_KHZ800);
+	mood_strip.begin();
+	//music.begin();
+}
+
+void cdefo::read_ndef()
+{
+	if (record_count > 0);
+	{
+		//POTENTIALY ADD MULTITHREAD METHODS HERE
+		Serial.println("Reading NDEF");
+		NdefRecord record = message.getRecord(rcounter++);
+
+		int payloadLength = record.getPayloadLength();
+		byte payload[payloadLength];
+		record.getPayload(payload);
+
+		String payloadAsString = ""; // Processes the message as a string vs as a HEX value
+		for (int c = 3; c < payloadLength; c++)
+		{
+			//POTENTIALLY ADD MULTITHREAD METHODS HERE
+			payloadAsString += (char)payload[c];
+		}
+		current_payload = payloadAsString;
+
+		record_count--; //decrements the number of records as we iterate through them
+	}
+}
+
+String cdefo::get_current_payload()
+{
+	return current_payload;
+}
+
+void cdefo::reset_payload()
+{
+	current_payload = "";
+}
+
 
 void cdefo::visit_website(String line)
 {
-	Serial.print(line);
+	if (current_payload != "")
+		Serial.print(line);
+	current_payload = "";
 }
 
 void cdefo::play_spotify(String song)
 {
-	Serial.print(song);
+	if (current_payload != "")
+		Serial.print(song);
+	current_payload = "";
 }
 
 void cdefo::play_video(String location)
@@ -25,36 +72,111 @@ void cdefo::play_audio(String location)
 {
 }
 
-void cdefo::chase(Adafruit_NeoPixel *strip, uint32_t c)
+void cdefo::mood_script(String script)
 {
-	Serial.print(":L:");
-	for (uint16_t i = 0; i < strip->numPixels() + 4; i++)
+	if (script != "") //if the method is called without script, then that means it is being run as part of a thread
 	{
-		strip->setPixelColor(i, c); // Draw new pixel
-		strip->setPixelColor(i - 4, 0); // Erase pixel a few steps back
-		strip->show();
-		delay(25);
+		mood += script;
+	}
+
+	//resets the payload
+	current_payload = "";
+
+	if (mood != "") //if the Arduino found a lighting script
+	{
+		//translate the script into actual lighting
+		//starts at i = 2 because of the ":L" at the beginning
+		for (int i = 2; i < mood.length(); i++)
+		{
+			//String mini = "";
+			//looks for the dividing character to split up the scripts
+			if (mood.charAt(i) == ';')
+			{
+				mood.remove(0, i + 1);
+				mini = mood.substring(0, mood.indexOf(';')); //makes a mini script from the first ";" to the next ";"
+				//adjust the index for the next iteration
+				i += mood.indexOf(';');
+				mood.remove(0, mood.indexOf(';')); //cleans up the main script string
+			}
+		}
 	}
 }
 
-void cdefo::stop(NfcAdapter *nfc)
+void cdefo::drive_lights()
+{
+	//process the mini script
+	for (int i = 1; i < mini.length(); i++)
+	{
+		//POTENTIALLY ADD MULTITHREAD METHODS HERE
+		uint32_t c = 0;
+		//Sets the color to be passed in
+		if (mini.charAt(i) == 'R')
+			//Red
+			c = Adafruit_NeoPixel::Color(255, 0, 0);
+		if (mini.charAt(i) == 'G')
+			//Green
+			c = Adafruit_NeoPixel::Color(0, 255, 0);
+		if (mini.charAt(i) == 'B')
+			//Blue
+			c = Adafruit_NeoPixel::Color(0, 0, 255);
+		if (mini.charAt(i) == 'Y')
+			//Yellow
+			c = Adafruit_NeoPixel::Color(255, 255, 0);
+		if (mini.charAt(i) == 'T')
+			//Turquoise
+			c = Adafruit_NeoPixel::Color(0, 255, 255);
+		if (mini.charAt(i) == 'P')
+			//Purple
+			c = Adafruit_NeoPixel::Color(255, 0, 255);
+		if (mini.charAt(i) == 'W')
+			//White
+			c = Adafruit_NeoPixel::Color(255, 255, 255);
+
+		//See which type of lighting script to plug the color into
+		if (mini.startsWith("C"))
+		{
+			chase(c);
+		}
+		Serial.println("exiting");
+	}
+}
+
+
+void cdefo::stop(NfcAdapter* nfc)
 {
 	boolean message = true;
 	while (nfc->tagPresent())
 	{
-		if (message) {
+		//MULTITHREADFUNCTIONS GO IN HERE
+		//drive_lights();
+		if (message)
+		{
 			Serial.println("Experience has finished executing, place the next tag :Stop:");
 			message = false;
 		}
 	}
 }
 
-void cdefo::run_records(NfcAdapter *nfc)
+
+void cdefo::write_records(NfcAdapter* nfc)
 {
-	
 }
 
-void cdefo::write_records(NfcAdapter *nfc)
+void cdefo::chase(uint32_t c)
 {
-	
+	Serial.print(":C:");
+
+	while (led_counter < mood_strip.numPixels() + 4)
+	{
+		unsigned long currentMillis = millis();
+		if (currentMillis - led_millis > INTERVAL_LED)
+		{
+			led_millis = currentMillis;
+			mood_strip.setPixelColor(led_counter, c); // Draw new pixel
+			mood_strip.setPixelColor(led_counter - 4, 0); // Erase pixel a few steps back
+			mood_strip.show();
+			led_counter++;
+		}
+	}
+	led_counter = 0;
 }
--- CDEFO_c++/CDEFO/CDEFO.h
+++ CDEFO_c++/CDEFO/CDEFO.h
@@ -6,29 +6,67 @@
 #include <Wire.h>
 #include <PN532_I2C.h>
 #include <PN532.h>   // The following files are included in the libraries Installed
+#include <TimedAction.h>
 
-#define PIN      6
+#define MOOD_PIN 6
+#define MUSIC_PIN 5
 #define N_LEDS 30
+#define INTERVAL_LED 25
 
 class cdefo
 {
 public:
-	cdefo();
+	cdefo(NfcTag *tag);
+
+	//Get/set methods
+	String get_current_payload();
+	void reset_payload();
 
 	//Read methods
-	static void chase(Adafruit_NeoPixel *strip, uint32_t c); //RGB lighting patterns
-	static void visit_website(String line);
-	static void play_spotify(String song);
+	void read_ndef(); //reads and stores the next payload in the Ndef message
+
+	//Script methods
+	void visit_website(String line);
+	void play_spotify(String song);
 	static void play_video(String location);
 	static void record_audio(String location);
 	static void play_audio(String location);
-	static void stop(NfcAdapter *nfc);
-	static void run_records(NfcAdapter *nfc);
+	void stop(NfcAdapter *nfc);
+
+	//Module methods
+	void mood_script(String script);
+	//drives all of the lights that are connected to the Arduino
+	void drive_lights();
+
 
 	//Write methods
 	static void write_records(NfcAdapter *nfc);
-private:
 
+private:
+	//Mood Lighting Patterns
+	void chase(uint32_t c);
+
+	//NeoPixel Strips
+	Adafruit_NeoPixel mood_strip;
+	//Adafruit_NeoPixel music;
+
+	NdefMessage message;
+	// If you have more than 1 Message then it wil cycle through them
+	int record_count = 0;
+	int rcounter = 0;
+
+	//used to keep track of how far into the LED script it is
+	int led_counter = 0;
+
+	//user for timing of LEDS
+	long led_millis = 0;
+
+	//TURN THESE INTO CHARACTER ARRAYS TO HELP SAVE RAM
+	String current_payload = "";
+
+	//stores the mood lighting script in here so that it can run until the tag is removed
+	String mood = "";
+	String mini = "";
 };
 
 #endif
