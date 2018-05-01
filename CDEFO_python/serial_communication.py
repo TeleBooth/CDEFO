ba89d03c708c1b0a6136848fd0d82c27a0d18ac4
TeleBooth <jbooth1@terpmail.umd.edu>
2018-05-01 18:29:15 -0400

    Added GUI and threading that allows it to work

--- CDEFO_python/serial_communication.py
+++ CDEFO_python/serial_communication.py
@@ -1,4 +1,6 @@
 import serial
+import threading
+from time import sleep
 import webbrowser
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -6,6 +8,7 @@ import pprint
 import sounddevice as sd
 import soundfile as sf
 import numpy
+from tkinter import *
 
 import sys
 
@@ -14,82 +17,137 @@ ser = serial.Serial(
     baudrate=9600
 )
 
-out = ""
-
 # default samplerate and channels
 sd.default.samplerate = 44100
 sd.default.channels = 2
 
-recording = []
+out = ""
+lock = threading.Lock()
 
-#used for recording to the soundfile
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
-    q.put(indata.copy())
+# Serial read thread
+def handle_data(data):
+    global out
+    lock.acquire()
+    out = data
+    lock.release()
 
 
 
-#Spotify credentials
+def read_from_port(port):
+    while True:
+        reading = port.readline().decode("utf-8")
+        print("Reading: " + reading)
+        handle_data(reading)
+
+
+ser_thread = threading.Thread(target=read_from_port, args=(ser,))
+
+# Spotify credentials
 client_credentials_manager = SpotifyClientCredentials(client_id='0f65027d2c8e42bc9499eb12e885cb62',
-                           client_secret='314f354ce98344ac9cb22f6a4c50af59')
+                                                      client_secret='314f354ce98344ac9cb22f6a4c50af59')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+
+# TKinter widgets
+class Fullscreen_Window:
+
+    def __init__(self):
+        self.state = False
+        self.tk = Tk()
+        self.frame = Frame(self.tk)
+        self.tk.attributes("-topmost", True)
+        self.tk.attributes("-fullscreen", self.state)
+        self.experience = Text(self.tk, height=1, width=50)
+        self.experience.pack()
+
+        self.frame.pack()
+        self.state = False
+        self.tk.bind("<F11>", self.toggle_fullscreen)
+        self.tk.bind("<Escape>", self.end_fullscreen)
+
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state  # Just toggling the boolean
+        self.tk.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.tk.attributes("-fullscreen", False)
+        return "break"
+
+
+w = Fullscreen_Window()
+ser_thread.start()
+
 while 1:
-    out = ser.readline().decode("utf-8")
-    #out = input('Enter your input:')
-    #print(ser.read().decode("utf-8"))
+    # out = ser.readline(timeout=0).decode("utf-8")
+    # out = ""
+    # print(ser.readline().decode("utf-8"))
+    w.tk.update()
 
     if "Firmware ver. 1.6" in out:
-        print(out, flush=True)
+        w.experience.insert('0.0', out)
+        lock.acquire
         out = ""
+        lock.release
 
     if "place a tag." in out:
-        print(out)
+        lock.acquire
         out = ""
+        lock.release
 
     if ":L:" in out:
         print("found lights")
+        lock.acquire
         out = ""
+        lock.release
 
     if ":W:" in out:
         print("parsing website:")
+        lock.acquire
         out = ""
+        lock.release
 
     if ":We:" in out:
-        print("going to website:")
-        print(out.replace(":We:", ""))
-        webbrowser.open(out.replace(":We:", ""), new=0, autoraise=True)
+        w.experience.insert('0.0', "going to website:" + out.replace(":We:", ""))
+        webbrowser.open(out.replace(":We:", ""), new=1, autoraise=False)
+        lock.acquire
         out = ""
+        lock.release
 
     if ":S:" in out:
-        print("searching for Spotify artist:")
-        print(out.replace(":S:", ""))
+        w.experience.insert('0.0', "Searching for Spotify artist:" + out.replace(":S:", ""))
+        # print("searching for Spotify artist:")
+        # print(out.replace(":S:", ""))
         result = sp.search(out.replace(":S:", ""))
         for key in result['tracks']['items']:
-            pprint.pprint(key['name'], depth = 1)
+            pprint.pprint(key['name'], depth=1)
             pprint.pprint(key['external_urls']['spotify'])
 
         trim = result['tracks']['items'][0]['external_urls']['spotify']
-        webbrowser.open(trim.replace("'", ""), new=0, autoraise=True)
+        webbrowser.open(trim.replace("'", ""), new=1, autoraise=False)
+        lock.acquire
         out = ""
+        lock.release
 
     if ":R:" in out:
         duration = [int(s) for s in out.split(":") if s.isdigit()]
+        print(duration)
         print("Recording for " + str(duration[0]) + " seconds.")
-        recording = sd.rec(int(duration[0] * 44100))
-        sd.wait()
         print("Finished")
-        out = ""
+        # if ":P:" in out:
+        #     sd.wait()
 
-        with sf.SoundFile("CDEFO.wav", mode='x', samplerate = 44100, channels = 2, subtype = "PCM_24") as file:
-            file.write(recording)
-
-    if ":P:" in out:
-        sd.play(recording)
+        # with sf.SoundFile("CDEFO.wav", mode='x', samplerate=44100, channels=2, subtype="PCM_24") as file:
+        #     file.write(recording)
+        lock.acquire
         out = ""
+        lock.release
 
-    if ":Stop:" in out:
-        print(out.replace(":Stop:",""))
+    if ":End:" in out:
+        lock.acquire
         out = ""
+        lock.release
+
+    if "Cleanup finished :Stop:" in out:
+        w.experience.insert('0.0', out)
